f784d330e40e4ddf33e358940e7f85e08d2be4ab
VangaPriyanka <priyankavanga44@gmail.com>
2026-04-14 07:10:41 +0530

    Day 7 : Improvements

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express"; // ✅ ADD THIS
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import restaurantRoutes from "./routes/restaurantRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+
 
 dotenv.config();
 
@@ -10,6 +12,8 @@ const app = express();
 app.use(express.json());
 
 app.use("/api/restaurants", restaurantRoutes);
+app.use("/api/users", userRoutes);
+
 
 // CONNECT DATABASE
 mongoose.connect(process.env.MONGO_URI)
@@ -25,4 +29,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`🔥 Server running on port ${PORT}`);
+  console.log("User routes loaded");
+
 });
